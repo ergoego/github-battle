e366a7d6085b13de16b4919dcfdc1c656c16d679
Erik Olson <ergoego@users.noreply.github.com>
2018-01-02 22:04:48 -0800

    API fleshed out to complete requests to github needed for battle.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ var Popular = require('./Popular.js');
 var Battle = require('./Battle.js');
 var Nav = require('./Nav.js');
 var Home = require('./Home.js');
+var Results = require('./Results.js');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
@@ -18,6 +19,7 @@ class App extends React.Component {
           <Route exact path='/' component={Home}/>
           <Route path='/popular' component={Popular}/>
           <Route exact path='/battle' component={Battle}/>
+          <Route path='/battle/results' component={Results}/>
           <Route render={function(){return<p>404 Not Found</p>}}/>
           </Switch>
         </div>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,67 @@
-var axios = require('axios')
+var axios = require('axios');
+
+//The below is created with github so that we don't get locked out of the github api for making excessive requests. The params get appended to the request so that we can continue making requests.
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var params = "?client_id=" + id + "&client_secret=" + sec;
+
+function getProfile(username) {
+  return axios.get('https://api.github.com/users/' + username + params)
+    .then(function(user) {
+      return user.data;
+    });
+}
+
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
+}
+
+function getStarCount(repos) {
+  return repos.data.reduce(function(count, repo) {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+
+function calculateScore(profile, repos) {
+  var followers = profile.followers;
+  var totalStars = getStarCount(repos);
+
+  return (followers * 3) + totalStars;
+}
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+
+function getUserData(player) {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(function(data) {
+    // axios.all goes and does these things - getProfile and getRepos - and when they are all done, it will come back and that array - data - will enter this function. data[0] is the first promise, and data[n] is the nth promise that we entered into the array argument for axios.all.
+    var profile = data[0];
+    var repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    }
+  });
+}
+
+function sortPlayers(players) {
+  return players.sort(function(a,b) {
+    return b.score - a.score;
+  });
+}
 
 module.exports = {
+  battle: function (players) {
+    return axios.all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError)
+  },
   fetchPopularRepos: function (language) {
     var encodeURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
